8558de10cac509006a16dc5ebc191e688f41fbb9
Sean Clark <seaniboy2009@yahoo.co.uk>
2022-07-19 12:46:44 +0000

    change removed repeaded code and made it smaller

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,48 +32,43 @@ function populateQuestion() {
     let currentQuestion = document.getElementById("question-number").innerText;
     let newQuestion;
     let newAnswer;
-    let wrongAnswer;
 
+    let wrongAnswer1;
+    let wrongAnswer2;
+    let wrongAnswer3;
+    let wrongAnswer4;
+
+    /** go through the current question number and then set it to the next question */
     switch (currentQuestion) {
         case "0":
             document.getElementById("question-number").innerText = "1";
-            newQuestion = listOfQuestions().question;
-            newAnswer = listOfQuestions().answer;
-            wrongAnswer = listOfQuestions().wrongAnswer;
             break;
         case "1":
             document.getElementById("question-number").innerText = "2";
-            newQuestion = listOfQuestions().question;
-            newAnswer = listOfQuestions().answer;
-            wrongAnswer = listOfQuestions().wrongAnswer;
             break;
         case "2":
             document.getElementById("question-number").innerText = "3";
-            newQuestion = listOfQuestions().question;
-            newAnswer = listOfQuestions().answer;
-            wrongAnswer = listOfQuestions().wrongAnswer;
             break;
         case "3":
             document.getElementById("question-number").innerText = "4";
-            newQuestion = listOfQuestions().question;
-            newAnswer = listOfQuestions().answer;
-            wrongAnswer = listOfQuestions().wrongAnswer;
             break;
         case "4":
             document.getElementById("question-number").innerText = "5";
-            newQuestion = listOfQuestions().question;
-            newAnswer = listOfQuestions().answer;
-            wrongAnswer = listOfQuestions().wrongAnswer;
             break;
         case "5":
-            document.getElementById("question-number").innerText = "5";
-            newQuestion = listOfQuestions().question;
-            newAnswer = listOfQuestions().answer;
-            wrongAnswer = listOfQuestions().wrongAnswer;
+            document.getElementById("question-number").innerText = "";
+            quizCompleted();
             break;
         default:
             break;
     }
+
+    newQuestion = listOfQuestions().question;
+    newAnswer = listOfQuestions().answer;
+    wrongAnswer1 = listOfQuestions().wrongAnswer1;
+    wrongAnswer2 = listOfQuestions().wrongAnswer2;
+    wrongAnswer3 = listOfQuestions().wrongAnswer3;
+    wrongAnswer4 = listOfQuestions().wrongAnswer4;
 
     /** Change the question */
     document.getElementById("question-text").innerText = newQuestion;
@@ -83,18 +78,16 @@ function populateQuestion() {
     console.log(random);
 
     /** Change the answers */
-    document.getElementById("answer-box1").innerText = random == 0 ? newAnswer : wrongAnswer;
-    document.getElementById("answer-box2").innerText = random == 1 ? newAnswer : wrongAnswer;
-    document.getElementById("answer-box3").innerText = random == 2 ? newAnswer : wrongAnswer;
-    document.getElementById("answer-box4").innerText = random == 3 ? newAnswer : wrongAnswer;
-
+    document.getElementById("answer-box1").innerText = random == 0 ? newAnswer : wrongAnswer1;
+    document.getElementById("answer-box2").innerText = random == 1 ? newAnswer : wrongAnswer2;
+    document.getElementById("answer-box3").innerText = random == 2 ? newAnswer : wrongAnswer3;
+    document.getElementById("answer-box4").innerText = random == 3 ? newAnswer : wrongAnswer4;
 }
 
 /** check the answer the user has selected is the correct one for the question that is displayed */
 function checkAnswer(button) {
 
     let currentQuestion = document.getElementById("question-number").innerText;
-
     let userAnswer = button.innerText;
 
     switch (currentQuestion) {
@@ -104,6 +97,7 @@ function checkAnswer(button) {
             } else {
                 incorrectAnswer();
             }
+            populateQuestion();
             break;
         case "2":
             if (userAnswer === questions[1].answer) {
@@ -112,6 +106,7 @@ function checkAnswer(button) {
             } else {
                 incorrectAnswer();
             }
+            populateQuestion();
             break;
         case "3":
             if (userAnswer === questions[2].answer) {
@@ -120,6 +115,7 @@ function checkAnswer(button) {
             } else {
                 incorrectAnswer();
             }
+            populateQuestion();
             break;
         case "4":
             if (userAnswer === questions[3].answer) {
@@ -128,6 +124,7 @@ function checkAnswer(button) {
             } else {
                 incorrectAnswer();
             }
+            populateQuestion();
             break;
         case "5":
             if (userAnswer === questions[4].answer) {
@@ -136,24 +133,40 @@ function checkAnswer(button) {
             } else {
                 incorrectAnswer();
             }
+            populateQuestion();
             break;
         default:
             break;
     }
-
-    populateQuestion();
 }
 
 function correctAnswer() {
     let correct = document.getElementById("correct").innerText;
     document.getElementById("correct").innerText = ++correct;
 
+    document.getElementById("correct-incorrect").style = "color:green";
+    document.getElementById("correct-incorrect").innerText = "Correct!";
 }
 
 function incorrectAnswer() {
     let incorrect = document.getElementById("incorrect").innerText;
     document.getElementById("incorrect").innerText = ++incorrect;
 
+    document.getElementById("correct-incorrect").style = "color:red";
+    document.getElementById("correct-incorrect").innerText = "InCorrect!";
+}
+
+function quizCompleted() {
+
+    let correct = document.getElementById("correct").innerText;
+
+    document.getElementById("question-text").style.display = "none";
+    document.getElementById("question-heading").innerText = `Congradualtions you have completed the quiz and got ${correct} out of ${questions.length}`;
+
+    document.getElementById("answer-box1").style.display = "none";
+    document.getElementById("answer-box2").style.display = "none";
+    document.getElementById("answer-box3").style.display = "none";
+    document.getElementById("answer-box4").style.display = "none";
 }
 
 /** check the current question number and return the new question */
@@ -186,6 +199,12 @@ function listOfQuestions() {
 }
 
 function resetGame() {
+
+    document.getElementById("question-text").style.display = "";
+    document.getElementById("answer-box1").style.display = "";
+    document.getElementById("answer-box2").style.display = "";
+    document.getElementById("answer-box3").style.display = "";
+    document.getElementById("answer-box4").style.display = "";
     document.getElementById("question-number").innerText = "0";
     document.getElementById("correct").innerText = "0";
     document.getElementById("incorrect").innerText = "0";
@@ -195,26 +214,41 @@ function resetGame() {
 let questions = [{
         question: "what do they call the invisible power that binds the galaxy together?",
         answer: "The Force",
-        wrongAnswer: "The Magic"
+        wrongAnswer1: "The Magic",
+        wrongAnswer2: "The Magic",
+        wrongAnswer3: "The Magic",
+        wrongAnswer4: "The Magic",
     },
     {
         question: "C-3P0 is fluent in how many languages?",
         answer: "Over 60 million languages",
-        wrongAnswer: "Over 100 million languages"
+        wrongAnswer1: "The Magic",
+        wrongAnswer2: "The Magic",
+        wrongAnswer3: "The Magic",
+        wrongAnswer4: "The Magic",
     },
     {
         question: "Who killed the four Jedi Masters: Saesee Tinn, Mace Windu, Kit Fisto, and Agen Kolar?",
         answer: "Darth Sidious",
-        wrongAnswer: "Darth Vader"
+        wrongAnswer1: "The Magic",
+        wrongAnswer2: "The Magic",
+        wrongAnswer3: "The Magic",
+        wrongAnswer4: "The Magic",
     },
     {
         question: "What is the name of Yoda’s home?",
         answer: "Dagobah",
-        wrongAnswer: "Yavin"
+        wrongAnswer1: "The Magic",
+        wrongAnswer2: "The Magic",
+        wrongAnswer3: "The Magic",
+        wrongAnswer4: "The Magic",
     },
     {
         question: "Who is the young Jedi Knight who becomes Darth Vader?",
         answer: "Anakin Skywalker",
-        wrongAnswer: "Luke Skywalker"
+        wrongAnswer1: "The Magic",
+        wrongAnswer2: "The Magic",
+        wrongAnswer3: "The Magic",
+        wrongAnswer4: "The Magic",
     }
 ]
